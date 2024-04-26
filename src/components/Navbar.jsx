058ececaca894f719f611ae27f50714608563975
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-04-26 06:29:50 -0700

    feat: Navbar Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-        
+    <div className='text-white'>
+        <h1 className='w-full text -3xt font-bold text-[green]'>Landingpage</h1>
+        <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
     </div>
   )
 }
