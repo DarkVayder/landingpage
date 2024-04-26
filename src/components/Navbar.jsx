be7033973e7031db166b2ebe076626123b6f2979
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-04-26 13:22:24 -0700

    feat: Navbar Mobile view menu styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 function Navbar() {
+   const [nav, setNav] = useState(false)
+
+   const handleNav = () => {
+    setNav(!nav)
+   }
+
+
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='w-full text -3xt font-bold text-[green]'>CreativeLogo</h1>
-        <ul className='flex'>
+        <ul className='flex hidden'>
             <li className='p-4'>HOME</li>
             <li className='p-4'>SERVICES</li>
             <li className='p-4'>PAGES</li>
@@ -12,6 +20,23 @@ function Navbar() {
             <li className='p-4'>BLOG</li>
             <li className='p-4'>CONTENT</li>
         </ul>
+
+        <di onClick={handleNav}>
+           {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size = {20} />}
+        </di>
+
+        <div className={!nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 bg-#424442 ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        
+          <ul className='pt-14 uppercase p-4 cursor-pointer'>
+            <li className='p-4 border-b border-gray-600'>HOME</li>
+            <li className='p-4 border-b border-gray-600'>SERVICES</li>
+            <li className='p-4 border-b border-gray-600'>PAGES</li>
+            <li className='p-4 border-b border-gray-600'>CASE SHOP</li>
+            <li className='p-4 border-b border-gray-600'>BLOG</li>
+            <li className='p-4 border-b border-gray-600'>CONTENT</li>
+          </ul>
+        </div>
+
     </div>
   )
 }
