93593f46133f3aebc3dcaa51f0d516158c0a4c12
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-04-26 15:51:06 -0700

    feat: Hero Text flex

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Hero() {
   return (
     <div className='text-white'>
-      <div className='fixed left-0 top-0 w-full h-screen flex items-center justify-center'>
-        <div className="text-left max-w-4xl mx-auto px-6">
-          <p className="text-base lg:text-lg mb-4">WELCOME TO CODESHAPER</p>
+      <div className='max-w-4xl mx-auto mt-[-96px] h-screen flex items-center justify-center'>
+        <div className="text-left">
+          <p className="text-lg mb-4">WELCOME TO CODESHAPER</p>
           <div className="text-4xl md:text-5xl lg:text-6xl font-bold">
             CONSULTING <br /> FOR EVERY <br /> BUSINESS
           </div>
