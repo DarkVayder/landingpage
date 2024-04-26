ae36cde75e43e7c1bcd850c91ae3e47ac007f873
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-04-26 16:06:14 -0700

    feat: Added Button to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 function Hero() {
   return (
     <div className='text-white'>
-      <div className='max-w-4xl mx-auto mt-[-96px] h-screen flex items-center justify-center'>
-        <div className="text-left">
+      <div className='max-w-4xl mx-auto mt-[-96px] h-screen flex flex-col items-center justify-center'>
+        <div className="text-left mb-8">
           <p className="text-lg mb-4">WELCOME TO CODESHAPER</p>
           <div className="text-4xl md:text-5xl lg:text-6xl font-bold">
             CONSULTING <br /> FOR EVERY <br /> BUSINESS
           </div>
         </div>
+        <button className='bg-[blue] w-[180px] rounded-md font-medium py-2 px-6'>DISCOVER MORE</button>
       </div>
     </div>
   );
