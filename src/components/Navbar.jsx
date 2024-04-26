a2abacd8af3e5e6ec3c806fe13e29f69e01a3f60
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-04-26 13:27:19 -0700

    Feat: Completed Nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='w-full text -3xt font-bold text-[green]'>CreativeLogo</h1>
-        <ul className='flex hidden'>
+        <ul className='hidden md:flex'>
             <li className='p-4'>HOME</li>
             <li className='p-4'>SERVICES</li>
             <li className='p-4'>PAGES</li>
@@ -21,7 +21,7 @@ function Navbar() {
             <li className='p-4'>CONTENT</li>
         </ul>
 
-        <di onClick={handleNav}>
+        <di onClick={handleNav} className='block md:hidden'>
            {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size = {20} />}
         </di>
 
