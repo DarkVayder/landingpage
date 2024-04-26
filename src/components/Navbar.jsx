71cb6f7be737681786b928f800767e940189af37
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-04-26 12:52:53 -0700

    feat: Navbar jsx options build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='text-white'>
-        <h1 className='w-full text -3xt font-bold text-[green]'>Landingpage</h1>
-        <ul>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+        <h1 className='w-full text -3xt font-bold text-[green]'>CreativeLogo</h1>
+        <ul className='flex'>
+            <li className='p-4'>HOME</li>
+            <li className='p-4'>SERVICES</li>
+            <li className='p-4'>PAGES</li>
+            <li className='p-4'>CASE SHOP</li>
+            <li className='p-4'>BLOG</li>
+            <li className='p-4'>CONTENT</li>
         </ul>
     </div>
   )
